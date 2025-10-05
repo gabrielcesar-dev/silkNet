9e463591a389e9f45d86e37abd5c4a4b62ccf39e
gabrielcesar-dev <142801180+gabrielcesar-dev@users.noreply.github.com>
2025-10-05 19:59:11 -0300

    feat: initialize project structure and add configuration and feature generation scripts

---
+++ silknet/config.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from dotenv import load_dotenv
+from loguru import logger
+
+# Load environment variables from .env file if it exists
+load_dotenv()
+
+# Paths
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+
+DATA_DIR = PROJ_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+EXTERNAL_DATA_DIR = DATA_DIR / "external"
+
+MODELS_DIR = PROJ_ROOT / "models"
+
+REPORTS_DIR = PROJ_ROOT / "reports"
+FIGURES_DIR = REPORTS_DIR / "figures"
+
+SPLIT_RATIO = 0.7  # Train/Val split ratio
+LEARNING_RATE = 0.01  # Learning rate for the optimizer
+BATCH_SIZE = 64  # Batch size for training and validation
+NUM_EPOCHS = 15  # Number of training epochs
+SEED = 42
+
+IMG_SIZE = 254  # Image size (height and width)
+
+DATASET_NAME = "hollow_knight_silksong_dataset"
+
+# If tqdm is installed, configure loguru with tqdm.write
+# https://github.com/Delgan/loguru/issues/135
+try:
+    from tqdm import tqdm
+
+    logger.remove(0)
+    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
+except ModuleNotFoundError:
+    pass
